dea06a7b6ce167dd012fb98248effcabe5ca9775
Theodore Golfinopoulos <golfit@mit.edu>
2013-09-26 15:31:09 -0400

    Put all code in top-level

--- myGit2/QcmMasterController/lut_n_tb.v
+++
@@ -1,9 +0,0 @@
-module test_lut_n();
-//This is a test-bench for the look-up table.  The point is to make sure that different instances of lut_n can
-//reference different lookup tables stored in the (`include-d) file, cornerNclk.v.
-//
-//Ted Golfinopoulos, 13 Mar 2012
-
-
-endmodule 
-
